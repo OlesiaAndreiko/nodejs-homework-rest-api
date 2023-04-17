fa71fc42ab9837b0590d22a3962d6c1a262eaa84
Olesia Andreiko <olesikmail@gmail.com>
2023-04-17 15:39:59 +0300

    added connect to MongoDB

--- server.js
+++ server.js
@@ -1,5 +1,20 @@
-const app = require('./app')
+const app = require("./app");
 
-app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000")
-})
+const mongoose = require("mongoose");
+
+const DB_HOST =
+  "mongodb+srv://Olesia:uQUeYLVprV9e9Gw1@cluster0.k16ocpe.mongodb.net/db-contacts?retryWrites=true&w=majority";
+
+mongoose
+  .connect(DB_HOST)
+  .then(() =>
+    app.listen(3000, () => {
+      console.log("Database connection successful");
+    })
+  )
+  .catch((error) => {
+    console.log(error.message);
+    process.exit(1);
+  });
+
+// uQUeYLVprV9e9Gw1
